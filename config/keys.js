7a5cb98bc81a969f6b7d33ab7e1d41e8d90e650a
Justin McLaren <justinmclaren@Justins-MacBook-Pro.local>
2017-11-20 13:37:48 -0800

    production

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '449492557573-2rg6lidb43eliboajupk3jhedujf7c5o.apps.googleusercontent.com',
+  googleClientSecret: 'd7sUjCWr1CG5bz1RgVPLiIk2',
+  cookieKey: 'asdfsdlfkjasdlfjkasd;lfjkasdlkfj',
+  mongoURI: 'mongodb://root:password@ds113636.mlab.com:13636/baz-dev'
+};
